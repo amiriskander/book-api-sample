dd9c62a1247d1018f22b1a1f4946e7b634cc0aac
Amir Iskander <amir@amiriskander.com>
2017-10-15 00:18:15 +0200

    Use the new producer and consumer

--- www/book_api/src/AppBundle/Controller/Api/BookController.php
+++ www/book_api/src/AppBundle/Controller/Api/BookController.php
@@ -2,11 +2,14 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Consumer\CreateBookConsumer;
+use AppBundle\Producer\CreateBookProducer;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use PhpAmqpLib\Connection\AMQPConnection;
 
 /**
  * @Route("/api/book")
@@ -18,6 +21,26 @@ class BookController extends FOSRestController
      */
     public function createAction(Request $request)
     {
+        /*$params = array(
+            'host' => 'rabbit',
+            'port' => 5672,
+            'username' => 'rabbit',
+            'password' => 'mq',
+            'vhost' => '/'
+        );
+
+        $connection = new AMQPConnection(
+            $params['host'],
+            $params['port'],
+            $params['username'],
+            $params['password'],
+            $params['vhost']
+        );
+
+        dump($connection->isConnected());
+
+        dump($connection); die;*/
+
         $bookData = $request->request->all();
 
 //        dump($bookData);
@@ -28,10 +51,28 @@ class BookController extends FOSRestController
         // dump( class_exists(CreateBookProducer::class) ); die;
 
         // $this->get('create_book')->enqueue(serialize($bookData));
-        $this->get('old_sound_rabbit_mq.create_book_producer')->publish(serialize($bookData));
+        // $this->get('old_sound_rabbit_mq.create_book_producer')->publish(serialize($bookData));
+
+        $producer = new CreateBookProducer();
+        $producer->enqueue($bookData);
 
         die;
 
     }
 
+    /**
+     * @Rest\Get("/search", name="book_api_search")
+     */
+    public function searchAction(Request $request)
+    {
+        $consumer = new CreateBookConsumer();
+        $consumer->listen();
+        die;
+//        $finder = $this->get('foq_elastica.finder.library.book');
+//        // $searchTerm = $request->query->get('search');
+//        $searchTerm = 'Amir';
+//        $sites = $finder->find($searchTerm);
+//        dump($sites); die;
+//        return array('sites' => $sites);
+    }
 }
