9a6db6eb503ecc09d7e8af8e344da926961a59ca
Amir Iskander <amir@amiriskander.com>
2017-10-21 00:15:04 +0200

    Book Entity class -- will remove it for the favor of Book Document

--- www/book_api/src/AppBundle/Entity/Book.php
+++ www/book_api/src/AppBundle/Entity/Book.php
@@ -3,9 +3,10 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * ORM\Entity
+ * @ORM\Entity
  */
 class Book
 {
@@ -19,14 +20,18 @@ class Book
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
-     * @Assert\MaxLength(255)
+     * @Assert\Length(
+     *      max = 255
+     * )
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
-     * @Assert\MaxLength(255)
+     * @Assert\Length(
+     *      max = 255
+     * )
      */
     private $author;
 
@@ -38,7 +43,61 @@ class Book
     /**
      * @ORM\Column(type="string", length=13)
      * @Assert\NotBlank()
-     * @Assert\MaxLength(13)
+     * @Assert\Length(
+     *     min = 13,
+     *     max = 13,
+     *     exactMessage = "Book ISBN must be 13 characters long."
+     * )
      */
     private $isbn;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+        return $this;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function setYear($year)
+    {
+        $this->year = new \DateTime();
+        $this->year->setDate($year, 1, 1);
+        return $this;
+    }
+
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+        return $this;
+    }
 }
